ce0d2bb7bafa8b0f38e58ca8ca3a8464a3db082b
Sergei Novoselov <Strifer@inbox.ru>
2026-09-20 03:37:12 +0300

    Дополнил выполнение 4го домашнего задания

--- script.js
+++ script.js
@@ -4,15 +4,37 @@ let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?');
 let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let adaptive = confirm('Нужен ли адаптив на сайте?');
+
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-let rollback = 10;
 
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+
+const getAllServicePrices = function () {
+    return servicePrice1 + servicePrice2
+}
 
 const showTypeOf = function (variable) {
     console.log(variable, typeof variable);
+}
+
+const getFullPrice = function () {
+    return screenPrice + allServicePrices
+}
+
+const getServicePercentPrices = function () {
+    return Math.ceil(fullPrice - (fullPrice * (rollback / 100)))
+}
+
+const getTitle = function () {
+    if (!title) return ''
+    if (title.length === 0) return ''
+    return title.trim()[0].toUpperCase() + title.trim().slice(1).toLowerCase();
 }
 
 const getRollbackMessage = function (price) {
@@ -28,29 +50,10 @@ const getRollbackMessage = function (price) {
     }
 }
 
-const getAllServicePrices = function (servicePrice1, servicePrice2) {
-    return servicePrice1 + servicePrice2
-}
-
-const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-
-function getFullPrice(screenPrice, allServicePrices) {
-    return screenPrice + allServicePrices
-}
-
-const fullPrice = getFullPrice(screenPrice, allServicePrices)
-
-function getTitle(str) {
-    if (!str) return ''
-    if (str.length === 0) return ''
-    return str[0].toUpperCase() + str.slice(1).toLowerCase();
-}
-
-function getServicePercentPrices() {
-    return Math.ceil(fullPrice - (fullPrice * (rollback / 100)))
-}
-
-const servicePercentPrice = getServicePercentPrices(fullPrice)
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice()
+servicePercentPrice = getServicePercentPrices()
+title = getTitle()
 
 
 showTypeOf(title)
@@ -60,3 +63,5 @@ showTypeOf(adaptive)
 console.log(screens);
 console.log(getRollbackMessage(fullPrice));
 console.log(servicePercentPrice);
+
+console.log(`Стоимость верстки экранов ${screenPrice} юани и Стоимость разработки сайта ${fullPrice} юани`);
